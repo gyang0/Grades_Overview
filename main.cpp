62ee003c56675cda3b962f89e870842b829e1d38
lightdrizzle <107653576+lightdrizzle@users.noreply.github.com>
2022-06-21 21:14:29 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,51 @@
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
+    // File
+    fstream file;
+    
+    file.open("grades.txt");
+    if(file.fail())
+    {
+        cout << "Failed to open file.";
+        exit(1);
+    }
+    
+    
     int choice = 0;
     
     cout << "Academic Scores\n";
     cout << "Pick one:\n";
     cout << "   1. Add a new course\n";
-    cout << "   3. Delete a course\n";
-    cout << "   2. See overview\n";
+    cout << "   2. Delete a course\n";
+    cout << "   3. See overview\n";
     
     cin >> choice;
+    while(choice < 0 || choice > 3)
+    {
+        cout << "Try again: ";
+        cin >> choice;
+    }
+    
+    
+    switch(choice)
+    {
+        case 1:
+            // Add a course
+        break;
+        
+        case 2:
+            // Delete a course
+        break;
+        
+        case 3:
+            // See overview
+        break;
+    }
     
     
     return 0;
