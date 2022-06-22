65adba8c01d6b7cdaf7d876a5a8ad1f8af68bf94
lightdrizzle <107653576+lightdrizzle@users.noreply.github.com>
2022-06-21 18:43:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int choice = 0;
+    
+    cout << "Academic Scores\n";
+    cout << "Pick one:\n";
+    cout << "   1. Add a new course\n";
+    cout << "   3. Delete a course\n";
+    cout << "   2. See overview\n";
+    
+    cin >> choice;
+    
+    
+    return 0;
+}
