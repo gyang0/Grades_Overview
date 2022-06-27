f497bd7226184fd726a194015234a03ddad8f979
lightdrizzle <107653576+lightdrizzle@users.noreply.github.com>
2022-06-27 00:33:50 -0700

    Update funcs.cpp

--- funcs.cpp
+++ funcs.cpp
@@ -38,11 +38,16 @@ void addCourse()
     }
     
     // Echo back
-    std::cout << "\nAdded " << courseName << " (" << courseID << ") with a GPA of "
-         << gradeAverage << " to grade " << gradeTaken;
+    std::cout << "\nAdded course to file.";
+    
+    
+    std::string spaces;
     
     // Write to file
-    outputFile << gradeTaken << " " << gradeAverage << "                 [" << courseID << "] " << courseName << "\n";
+    outputFile << gradeTaken << spaces.assign(' ', 30)
+               << " [" << courseID << "] "
+               << courseName
+               << gradeAverage << "\n";
     
     
     outputFile.close();
@@ -57,6 +62,82 @@ void deleteCourse()
 
 void showOverview()
 {
+    std::string line;
+    int grade = 0,
+        count = 0,
+        word;
     
+    // For calculating average
+    double sumGrades = 0.0;
+    int numCourses = 0;
+    
+    
+    // File
+    std::ifstream file;
+    
+    
+    for(int i = 7; i <= 12; i++)
+    {
+        // Reset
+        sumGrades = 0.0;
+        numCourses = 0;
+        
+        
+        // Open file
+        file.open("grades.txt", std::ios::in);
+        
+        std::cout << i << "th grade\n";
+        std::cout << "  Course                        Grade\n";
+        
+        
+        
+        while(getline(file, line))
+        {
+            // The grade level
+            if(line[1] == ' ')
+                grade = ((int)line[0] - 48);
+            else
+                grade = ((int)line[0] - 48) * 10 + ((int)line[1] - 48);
+            
+            
+            if(grade == i)
+            {
+                numCourses++;
+                
+                // One digit
+                if(line[line.length() - 2] == ' ')
+                    sumGrades += (line[line.length() - 1] - 48) * 10;
+                
+                // Two digits
+                else if(line[line.length() - 3] == ' ')
+                    sumGrades += ((line[line.length() - 2] - 48) * 10
+                                  + (line[line.length() - 1] - 48));
+                // Three digits
+                else
+                    sumGrades += ((line[line.length() - 3] - 48) * 100
+                                  + (line[line.length() - 2] - 48) * 10
+                                  + (line[line.length() - 1] - 48));
+                
+                
+                std::cout << "  ";
+                for(int j = 2; j < line.length(); j++)
+                    std::cout << line[j];
+                    
+                std::cout << "\n";
+            }
+            
+        }
+        
+        if(numCourses > 0)
+            std::cout << "Average grade: " << (sumGrades/numCourses) << "\n";
+        else
+            std::cout << "No data\n";
+        
+        std::cout << "\n";
+        
+        // Close file
+        file.close();
+        
+    }
     
 }
