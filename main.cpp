e0cc4e4878360fab75e4f54592ef1614822313eb
G. Yang <107653576+gyang0@users.noreply.github.com>
2023-07-03 12:35:06 -0700

    Clean up code

--- main.cpp
+++ main.cpp
@@ -3,66 +3,56 @@
 #include <cstdlib>
 #include <stdlib.h>
 #include "funcs.h"
+#include "course.h"
 using namespace std;
 
 
-int main()
-{
-    // File
-    fstream file;
-    
-    char again = 'Y';
-    
-    // Check if file opens
-    file.open("grades.txt");
-    if(file.fail())
-    {
-        cout << "Failed to open file.";
-        exit(1);
-    }
-    file.close();
-    
-    
+int main(){
     // Main loop
-    do
-    {
+    do {
+        // Check for any updates of courses
+        updateCourseload();
+
+
         int choice = 0;
         
-        cout << "Academic Scores\n";
-        cout << "Pick one:\n";
+        cout << " __________________________\n";
+        cout << "|                           |\n";
+        cout << "|  Academic Scores Record   |\n";
+        cout << "|                           |\n";
+        cout << "----------------------------\n";
+        cout << "This is a program to help keep track of your academic scores.\n\n";
+        cout << "Enter one of the following choices:\n";
         cout << "   1. Add a new course\n";
         cout << "   2. Delete a course\n";
         cout << "   3. See overview\n";
+        cout << "   4. Quit program" << endl;
         
         cin >> choice;
-        while(choice < 0 || choice > 3)
-        {
-            cout << "Try again: ";
-            cin >> choice;
+        cout << endl;
+        
+        switch(choice){
+        case 1:
+            addCourse();
+            break;
+        case 2:
+            deleteCourse();
+            break;
+        case 3:
+            showOverview();
+            break;
+        case 4:
+            cout << "\n\nClosing program...\n";
+            exit(0);
+            break;
+        default:
+            while(choice < 0 || choice > 4){
+                cout << "Please try again with a number between 1 and 4: \n";
+                cin >> choice;
+            }
         }
         
-        
-        switch(choice)
-        {
-            case 1:
-                addCourse();
-            break;
-            
-            case 2:
-                deleteCourse();
-            break;
-            
-            case 3:
-                showOverview();
-            break;
-        }
-        
-        
-        cout << "\nBack to menu? (Y/N)\n";
-        cin >> again;
-        
-    } while(again == 'Y' || again == 'y');
-    
+    } while(true);
     
     
     return 0;
