1bcf27ef40ab0eb5ca92190d54fee7e90c7a4402
G. Yang <107653576+gyang0@users.noreply.github.com>
2024-01-09 19:42:04 -0800

    Switch to using semester grades

--- course.cpp
+++ course.cpp
@@ -1,7 +1,8 @@
+#include <utility>
 #include "course.h"
 
 // Constructor
-Course::Course(int _gradeTaken, std::string _courseCode, std::string _courseName, double _score){
+Course::Course(int _gradeTaken, std::string _courseCode, std::string _courseName, std::pair<double, double> _score){
 	gradeTaken = _gradeTaken;
 	courseCode = _courseCode;
 	courseName = _courseName;
@@ -12,10 +13,10 @@ Course::Course(int _gradeTaken, std::string _courseCode, std::string _courseName
 int Course::getGradeTaken(){ return gradeTaken; }
 std::string Course::getCourseCode(){ return courseCode; }
 std::string Course::getCourseName(){ return courseName; }
-double Course::getScore(){ return score; }
+std::pair<double, double> Course::getScore(){ return score; }
 
 // Setters
 void Course::setGradeTaken(int newVar){ gradeTaken = newVar; }
 void Course::setCourseCode(std::string newVar){ courseCode = newVar; }
 void Course::setCourseName(std::string newVar){ courseName = newVar; }
-void Course::setScore(int newVar){ score = newVar; }
+void Course::setScore(std::pair<double, double> newVar){ score = newVar; }
